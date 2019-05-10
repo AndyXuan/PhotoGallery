4b4962911f23153fbf01dbfe3e77e063fcf5a39c
xuandongdong <xuandong@juxiangfen.com>
2019-05-10 17:42:02 +0800

    1

--- app/src/main/java/me/xdd/self/photogallery/fragment/PhotoGalleryFragment.java
+++ app/src/main/java/me/xdd/self/photogallery/fragment/PhotoGalleryFragment.java
@@ -1,20 +1,33 @@
 package me.xdd.self.photogallery.fragment;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import me.xdd.self.photogallery.R;
+import me.xdd.self.photogallery.model.GalleryItem;
+import me.xdd.self.photogallery.net.FlickrFetchr;
 
 public class PhotoGalleryFragment extends Fragment {
+    private static final String TAG = "PhotoGalleryFragment";
     private RecyclerView mRecycleView;
-    public static PhotoGalleryFragment newFragment(){
+    private List<GalleryItem> mItems = new ArrayList<>();
+
+    public static PhotoGalleryFragment newFragment() {
         return new PhotoGalleryFragment();
     }
 
@@ -22,14 +35,77 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_photo_gallery,container,false);
+        View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
         mRecycleView = view.findViewById(R.id.photo_recycler_view);
-        mRecycleView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        mRecycleView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        setupAdapter();
         return view;
     }
+
+    private void setupAdapter() {
+        if (isAdded()) {
+            mRecycleView.setAdapter(new PhotoAdapter(mItems));
+        }
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, List<GalleryItem>> {
+
+        @Override
+        protected List<GalleryItem> doInBackground(Void... voids) {
+            return new FlickrFetchr().fetchItems();
+
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> galleryItems) {
+            mItems = galleryItems;
+            setupAdapter();
+        }
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+
+        public PhotoHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+
+        public void bindGalleryItem(GalleryItem item) {
+            mTitleTextView.setText(item.toString());
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+        private List<GalleryItem> mGralleryItems;
+
+        public PhotoAdapter(List<GalleryItem> mGralleryItems) {
+            this.mGralleryItems = mGralleryItems;
+        }
+
+        @NonNull
+        @Override
+        public PhotoHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            TextView textView = new TextView(getActivity());
+            return new PhotoHolder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PhotoHolder photoHolder, int i) {
+            GalleryItem item = mGralleryItems.get(i);
+            photoHolder.bindGalleryItem(item);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mGralleryItems.size();
+        }
+    }
+
 }
